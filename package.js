89dde309e2a708b8299c99c178b47497eb1caeb1
Christian Storch <ccstorch@gmail.com>
2016-07-05 14:37:01 +0100

    0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cinn:notifications',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Notifications collection and generator helpers',
   git: 'https://github.com/cinn-labs/meteor-notifications',
   documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   const both = ['client', 'server'];
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.4');
   api.export('Notifications');
   api.export('NotificationsHandler');
 
